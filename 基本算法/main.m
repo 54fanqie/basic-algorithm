6cf53e1a8a0a8d87359b5e42488b1957d33aa517
番茄 <fanqie@fanqiedeMacBook-Pro.local>
2017-09-21 22:54:04 +0800

    Initial Commit

---
+++ 基本算法/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  基本算法
+//
+//  Created by 番茄 on 2017/9/21.
+//  Copyright © 2017年 番茄. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
